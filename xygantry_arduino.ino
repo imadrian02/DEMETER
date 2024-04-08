2b9f254fbd26c2b2bc550c6ff8a08890ec70ca80
imadrian02 <adrian.jieern@gmail.com>
2024-04-08 19:08:16 +0800

    update

--- xygantry_arduino.ino
+++ xygantry_arduino.ino
@@ -5,7 +5,7 @@
 #define STEPS_PER_MM 5
 
 // Define the maximum speed and acceleration for your stepper motors
-#define MAX_SPEED_X 1500
+#define MAX_SPEED_X 2000
 #define MAX_SPEED_Y 2000
 #define ACCELERATION_X 1000
 #define ACCELERATION_Y 1000
@@ -21,7 +21,12 @@
 // Define the pin connected to the servo motor and limit switches
 #define Z_SERVO_PIN A3 // Changed servo pin to A3
 #define X_LIMIT_SWITCH_PIN 9
-#define Y_LIMIT_SWITCH_PIN 10
+#define Y1_LIMIT_SWITCH_PIN 10
+#define Y2_LIMIT_SWITCH_PIN 11
+
+#define X_MAX 1000
+#define Y_MAX 450
+
 
 // Define the coordinates for the home position
 float homeX = 0;
@@ -52,7 +57,8 @@ void setup() {
 
   // Set the limit switch pins as inputs with pullup resistors
   pinMode(X_LIMIT_SWITCH_PIN, INPUT_PULLUP);
-  pinMode(Y_LIMIT_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(Y1_LIMIT_SWITCH_PIN, INPUT_PULLUP);
+  pinMode(Y2_LIMIT_SWITCH_PIN, INPUT_PULLUP);
 
   // Move to home position
   moveGantryToHome();
@@ -65,7 +71,7 @@ void loop() {
     String input = Serial.readStringUntil('\n');
 
     // Check if the input is "RESET" and execute moveGantryToHome() function
-    if (input == "RESET") {
+    if (input == "R") {
       moveGantryToHome();
       Serial.println("Gantry moved to home position.");
     }
@@ -79,14 +85,21 @@ void loop() {
         float yTarget = input.substring(commaIndex1 + 1, commaIndex2).toFloat();
         int interval = input.substring(commaIndex2 + 1).toInt();
 
-        // Move the X and Y gantry to the target coordinates
-        moveGantry(xTarget, yTarget);
-        // Toggle the servo for the Z-axis
-        if (interval != 0){
-          toggleServo(interval);
+        if (xTarget <= X_MAX && yTarget <= Y_MAX){
+          // Move the X and Y gantry to the target coordinates
+          moveGantry(xTarget, yTarget);
+          // Toggle the servo for the Z-axis
+          if (interval != 0){
+            toggleServo(interval);
+          }
+
+          Serial.println("OK");
+        }else{
+          Serial.println("Out of Range");
         }
 
-        Serial.println("OK");
+
+
       }
     }
   }
@@ -141,7 +154,7 @@ void toggleServo(int interval) {
 void moveGantryToHome() {
   // Move X axis to home position
   stepperX.setMaxSpeed(1000); // Slow speed for homing
-  stepperX.setAcceleration(1000); // Slow acceleration for homing
+  stepperX.setAcceleration(2000); // Slow acceleration for homing
   while (digitalRead(X_LIMIT_SWITCH_PIN) == HIGH) {
     stepperX.moveTo(-10000); // Move in negative direction until limit switch is hit
     stepperX.run();
@@ -153,14 +166,18 @@ void moveGantryToHome() {
 
   // Move Y axes to home position
   stepperY1.setMaxSpeed(1000); // Slow speed for homing
-  stepperY1.setAcceleration(1000); // Slow acceleration for homing
+  stepperY1.setAcceleration(2000); // Slow acceleration for homing
   stepperY2.setMaxSpeed(1000); // Slow speed for homing
-  stepperY2.setAcceleration(1000); // Slow acceleration for homing
-  while (digitalRead(Y_LIMIT_SWITCH_PIN) == HIGH) {
+  stepperY2.setAcceleration(2000); // Slow acceleration for homing
+  while (digitalRead(Y1_LIMIT_SWITCH_PIN) == HIGH||digitalRead(Y2_LIMIT_SWITCH_PIN) == HIGH) {
     stepperY1.moveTo(-10000); // Move in negative direction until limit switch is hit
     stepperY2.moveTo(-10000); // Move in negative direction until limit switch is hit
-    stepperY1.run();
-    stepperY2.run();
+    if (digitalRead(Y1_LIMIT_SWITCH_PIN) == HIGH){
+      stepperY1.run();
+    }
+    if (digitalRead(Y2_LIMIT_SWITCH_PIN) == HIGH){
+      stepperY2.run();
+    }
     delay(1);
   }
   stepperY1.setCurrentPosition(0); // Set current position as home position
