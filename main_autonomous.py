af02d293db83029470dd14e7b90e3d9c3e9309b4
imadrian02 <adrian.jieern@gmail.com>
2024-05-12 23:02:55 +0800

    update

--- main_autonomous.py
+++ main_autonomous.py
@@ -2,6 +2,8 @@ import time
 import numpy as np
 import threading
 import cv2
+import math
+from wand.image import Image
 import serial
 from ultralytics import YOLO
 from supervision import ByteTrack, BoundingBoxAnnotator, LabelAnnotator, Detections
@@ -11,7 +13,7 @@ from videocapture import *
 
 init_time = time.time()
 
-model = YOLO(r'/home/pi/DEMETER/model/best.pt')
+model = YOLO(r'/home/pi/DEMETER/model/crop.pt')
 detector = GreenOnBrown(algorithm='exg')
 tracker = ByteTrack()
 
@@ -23,12 +25,9 @@ xygantry = XYGantry()
 
 ser = serial.Serial(r'/dev/ttyACM0', 115200)
 
-auto = input(f"Auto? [y/n] ")
-if auto.upper() == 'Y':
-    auto = True
-else:
-    auto = False
-# Declare weed_frame as global variable
+auto = True
+print("[INFO] Machine Initializing ...")
+
 global weed_frame
 global pause
 
@@ -47,10 +46,12 @@ def process_command():
 
             if res == "AUTO":
                 auto = True
+                pause = False
                 print("[INFO] Auto mode activated")
 
             if res == "MANUAL":
                 auto = False
+                pause = False
                 print("[INFO] Manual mode activated")
 
             if auto:
@@ -87,6 +88,8 @@ def get_centroid(x1, y1, x2, y2):
     return cx, cy
 
 
+dummy = input("[USER] Start routine? y/n ")
+
 estop_thread = threading.Thread(target=process_command)
 estop_thread.start()
 
@@ -102,9 +105,55 @@ try:
             pass
 
         frame = cap.read()
-        frame = frame[0:480, 0:640]
+        # frame = frame[0:480, 0:640]
 
-        results = model(frame, conf=0.8, verbose=False)
+        # # Convert OpenCV BGR format to RGB format expected by wand
+        # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # # Convert the frame to a wand image
+        # with Image.from_array(frame_rgb) as img:
+        #     # Apply barrel distortion correction
+        #     img.virtual_pixel = 'transparent'
+        #     img.distort('barrel', (0,0, 0.0, 0.0, 1.0))
+        #     frame = np.array(img)
+
+        # frame = frame[:, :, :3]
+
+        (h, w, _) = frame.shape
+
+        # set up the x and y maps as float32
+        map_x = np.zeros((h, w), np.float32)
+        map_y = np.zeros((h, w), np.float32)
+
+        scale_x = 1
+        scale_y = 1
+        center_x = w / 2
+        center_y = h / 2
+        radius = w / 2
+        # amount = -0.75   # negative values produce pincushion
+        amount = 0.1  # positive values produce barrel
+
+        # create map with the barrel pincushion distortion formula
+        for y in range(h):
+            delta_y = scale_y * (y - center_y)
+            for x in range(w):
+                # determine if pixel is within an ellipse
+                delta_x = scale_x * (x - center_x)
+                distance = delta_x * delta_x + delta_y * delta_y
+                if distance >= (radius * radius):
+                    map_x[y, x] = x
+                    map_y[y, x] = y
+                else:
+                    factor = 1.0
+                    if distance > 0.0:
+                        factor = math.pow(math.sin(math.pi * math.sqrt(distance) / radius / 2), amount)
+                    map_x[y, x] = factor * delta_x / scale_x + center_x
+                    map_y[y, x] = factor * delta_y / scale_y + center_y
+
+        # do the remap
+        frame = cv2.remap(frame, map_x, map_y, cv2.INTER_LINEAR)
+
+        results = model(frame, conf=0.01, verbose=False)
         crop_frame = results[0].plot()
         crop_boxes = results[0].boxes.xyxy.cpu().numpy()
 
@@ -130,10 +179,18 @@ try:
         weed_frame = box_annotator.annotate(frame.copy(), detections=detections)
         weed_frame = label_annotator.annotate(weed_frame, detections=detections, labels=labels)
 
+        # pause = True
+        # while pause:
+        #     pass
+
+        # count = 0
         for tracker_id, xyxy in zip(detections.tracker_id, detections.xyxy):
+            #     # if count > 3:
+            #     #     break
+            #     # count+=1
             x1, y1, x2, y2 = xyxy.tolist()
             x, y = get_centroid(x1, y1, x2, y2)
-            xygantry.job_update(target=tracker_id, x=x, y=y, treat_time=5)
+            xygantry.job_update(target=tracker_id, x=x, y=y, treat_time=1)
             xygantry.treat()
             print()
             history.append(tracker_id)
@@ -142,7 +199,9 @@ try:
             msg = 'OK'
             ser.write(msg.encode())
 
-            pause = True
+        # pause = True
+
+        dummy = input("[USER] Start routine? y/n ")
 
         print(f"[INFO] Treated {len(history)} weeds in {int(time.time() - start_time)} seconds")
         history.clear()
